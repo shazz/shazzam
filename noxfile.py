dd1e582bfd84502847752f1580423ecdc60ee3a9
Shazz <shazz.ng@gmail.com>
2021-01-25 11:28:02 -0500

    added nox session to install deps

--- noxfile.py
+++ noxfile.py
@@ -32,3 +32,59 @@ def docs(session):
 
     shutil.rmtree('docs/html', ignore_errors=True)
     session.run("pdoc3", "-o", "docs/html", "--html", "shazzam", "examples")
+
+@nox.session(python=False)
+def install_3rd_party(session):
+
+    # check deps
+    session.run("make", "--version")
+    session.run("cargo", "--version")
+    session.run("wget", "--version")
+    session.run("tar", "--version")
+    session.run("unzip", "--version")
+
+    shutil.rmtree('third_party', ignore_errors=True)
+
+    os.makedirs("third_party", exist_ok = True)
+    session.cd("third_party")
+
+    # cc65
+    session.run("git", "clone", "https://github.com/cc65/cc65.git")
+    session.cd("cc65")
+    session.run("make", "all")
+
+    # exomizer
+    session.cd("..")
+    os.makedirs("exomizer", exist_ok = True)
+    session.cd("exomizer")
+    session.run("wget", "https://bitbucket.org/magli143/exomizer/wiki/downloads/exomizer-3.1.0.zip")
+    session.run("unzip", "exomizer-3.1.0.zip")
+
+    # apultra
+    session.cd("..")
+    session.run("git", "clone", "https://github.com/emmanuel-marty/apultra.git")
+    session.cd("apultra")
+    session.run("make", "all")
+
+    # apultra
+    session.cd("..")
+    session.run("git", "clone", "https://github.com/emmanuel-marty/lzsa.git")
+    session.cd("lzsa")
+    session.run("make", "all")
+
+    # pucrunch
+    session.cd("..")
+    session.run("git", "clone", "https://github.com/mist64/pucrunch.git")
+    session.cd("pucrunch")
+    session.run("make", "all")
+
+    # nucrunch
+    session.cd("..")
+    session.run("wget", "https://csdb.dk/release/download.php?id=206619", "-O", "nucrunch.tgz")
+    session.run("tar", "-xvzf", "nucrunch.tgz")
+    session.run("mv", "nucrunch-1.0.1", "nucrunch")
+    session.cd("nucrunch")
+    session.run("make")
+
+
+
